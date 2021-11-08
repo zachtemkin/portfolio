ee076462046091622326874c14c0e5032aac90a6
Zachary Temkin <zachtemkin@gmail.com>
2021-11-08 07:35:49 -0500

    moves wip to dev

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,28 +3,25 @@ import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import IntroBlock from "../components/introBlock";
 import ProjectPreview from "../components/projectPreview";
-import { projectList } from "../components/layout.module.scss";
 
 const IndexPage = ({ data }) => {
   return (
     <Layout pageTitle='Zach Temkin, Product Designer'>
       <IntroBlock />
-      <div className={projectList}>
-        {data.allMdx.nodes.map((node) => (
-          <ProjectPreview
-            id={node.id}
-            slug={node.slug}
-            type={node.frontmatter.type}
-            title={node.frontmatter.title}
-            team={node.frontmatter.team}
-            time_frame={node.frontmatter.time_frame}
-            hero_image={
-              node.frontmatter.hero_image.childImageSharp.gatsbyImageData
-            }
-            tags={node.frontmatter.tags}
-          />
-        ))}
-      </div>
+      {data.allMdx.nodes.map((node) => (
+        <ProjectPreview
+          id={node.id}
+          slug={node.slug}
+          type={node.frontmatter.type}
+          title={node.frontmatter.title}
+          team={node.frontmatter.team}
+          time_frame={node.frontmatter.time_frame}
+          hero_image={
+            node.frontmatter.hero_image.childImageSharp.gatsbyImageData
+          }
+          tags={node.frontmatter.tags}
+        />
+      ))}
     </Layout>
   );
 };
