9c94f84ee63fb54a5074a7fd6f0933895156b2cd
Zachary Temkin <zachtemkin@gmail.com>
2021-12-07 22:06:45 -0500

    adds post for ar demo

--- src/components/projectBackground.jsx
+++ src/components/projectBackground.jsx
@@ -8,15 +8,15 @@ const ProjectBackground = (props) => {
   return (
     <section className='background-info'>
       <div className='row'>
-        <h2 className='background-header tablet-col-4 mobile-col-12'>
+        <h2 className='background-header desktop-col-4 mobile-col-12'>
           {props.backgroundHeading}
         </h2>
-        <div className='background-text tablet-col-8 mobile-col-12'>
+        <div className='background-text desktop-col-8 mobile-col-12'>
           {props.backgroundText}
         </div>
       </div>
       <div className='hero-image row'>
-        <div className='image-container tablet-col-8 mobile-col-12'>
+        <div className='image-container desktop-col-8 mobile-col-12'>
           <GatsbyImage image={props.heroImage} />
         </div>
       </div>
